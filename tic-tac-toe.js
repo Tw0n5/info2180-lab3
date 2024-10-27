72cfb0b636d237cf89f6a3070c2b3f294111e43d
Antwon <antwonfonglyewquee1@gmail.com>
2024-10-26 19:50:50 -0700

    Excerise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const square= document.querySelectorAll('.square');
+
+    square.forEach(function(square){
+        square.classList.add('.square');
+    })
+})
